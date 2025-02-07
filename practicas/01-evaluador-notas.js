93a81e1d7106eb293805726548d32723ecba7fc0
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-02-06 19:24:55 -0700

    Resolución de práctica de Evaluador de Notas con condicionales

--- practicas/01-evaluador-notas.js
+++ practicas/01-evaluador-notas.js
@@ -1,1 +1,16 @@
-nota = 
+nota = 100;
+
+if (nota < 0 || nota > 100) {
+    console.log("Valor de nota inválido.");
+} else {
+    if (nota >= 90) {
+        console.log("Estás APROBADO. ¡Excelente!");
+    } else if (nota >= 75 && nota <= 89) {
+        console.log("Estás APROBADO. ¡Bien!");
+    } else if (nota >= 60 && nota <= 74) {
+        console.log("Estás APROBADO. Suficiente.");
+    } else if (nota < 60) {
+        console.log("Estás REPROBADO. ¡Sorry!");
+    }
+}
+
