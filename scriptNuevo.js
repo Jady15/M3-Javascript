f9e624e0e3e83e03ba402ab58bbb996f28f92e4f
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-02-25 18:43:26 -0600

    Adaptación de scriptNuevo.js para utilizar una función flecha

--- scriptNuevo.js
+++ scriptNuevo.js
@@ -1,36 +1,28 @@
-document.addEventListener('DOMContentLoaded', iniciarApp);
-
-function iniciarApp(){
-    // Recuperando elementos del HTML
+document.addEventListener('DOMContentLoaded', () => {
     const productoInput = document.getElementById('producto');
     const agregarBtn = document.getElementById('agregar');
     const listaCompras = document.getElementById('lista-compras');
 
-    // Arreglo de productos
     let productos = [];
 
-    agregarBtn.addEventListener('click', function(){
-        // Al hacer click en el botón, se agregará el producto escrito en el input
-        agregarProducto(productos, productoInput, listaCompras);
-    }); 
-}
+    agregarBtn.addEventListener('click', () => {
+        const nombreProducto = productoInput.value.trim();
+        if (nombreProducto) {
+            const nuevoProducto = {
+                nombre: nombreProducto,
+                comprado: false
+            };
+            productos.push(nuevoProducto);
+            agregarProductoALista(nuevoProducto);
+            productoInput.value = '';
+        }
+    });
 
-function agregarProducto(productos, productoInput, listaCompras){
-    // Recuperando el valor ingresado en el input
-    const nombreProducto = productoInput.value.trim();
-    if(nombreProducto){ // Verifica que el input tenga contenido
-        const nuevoProducto = { // Objeto para declarar las características del nuevo producto
-            nombre: nombreProducto,
-            comprado: false
-        };
-        productos.push(nuevoProducto);  // Añadir el nuevo producto al arreglo
-        mostrarProductos(nuevoProducto, listaCompras);  // Llamada al método para mostrar el producto creado en el HTML
-        productoInput.value = '';   // Restablece el valor del input
+    function agregarProductoALista(producto) {
+        const nuevoItem = document.createElement('li');
+        nuevoItem.textContent = producto.nombre;
+        listaCompras.appendChild(nuevoItem);
     }
-}
+});
 
-function mostrarProductos(producto, listaCompras){
-    const nuevoItem = document.createElement('li'); // Crea un nuevo ListItem <li></li>
-    nuevoItem.textContent = producto.nombre;    // Escribe el nombre del producto dentro del ListItem como texto
-    listaCompras.appendChild(nuevoItem);        // Inserta el elemento <li> como último elemento de la lista <ul>
-}
+
