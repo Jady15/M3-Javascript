d46e98720bc0b6dafdbbe450540a9af332930cd0
Isaac Luviano <isaacluvianohernandez@gmail.com>
2025-02-27 15:26:09 -0600

    Resolución de práctica de DEVF sobre introducción al DOM (Simple Comment Box)

---
+++ practicas/06-caja-comentarios/main.js
@@ -0,0 +1,121 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const commentForm = document.getElementById("comment-form");
+    const commentInput = document.getElementById("comment-input");
+    const commentContainer = document.getElementById("comment-container");
+
+    let commentList = [];  // Lista de comentarios
+    let commentId = 1; // ID inicial
+    showComments(); // Muestra los comentarios actuales en pantalla
+
+    // Mostrar comentarios en pantalla
+    function showComments() {
+        commentContainer.innerHTML = "";    // Se limpia toda la sintaxis del contenedor de comentarios
+
+        if (commentList.length === 0){
+            const noCommentsDiv = document.createElement("div");
+            noCommentsDiv.classList.add('no-comments');
+            noCommentsDiv.textContent = "Aún no hay comentarios. ¡Escribe lo que quieras!";
+            commentContainer.appendChild(noCommentsDiv);
+            return;
+            /*----------------------------------------------
+            <div class="no-comments">
+            Aún no hay comentarios. ¡Escribe lo que quieras!
+            </div>
+            -----------------------------------------------*/ 
+        }
+        commentList.forEach(comment => {
+            const commentWrapper = document.createElement("div");
+            commentWrapper.classList.add("comment-wrapper");
+            /*----------------------------------------------
+            <div class="comment-wrapper">
+            </div>
+            -----------------------------------------------*/
+
+            const commentDiv = document.createElement("div");
+            commentDiv.classList.add("comment");
+
+            /*----------------------------------------------
+            <div class="comment">
+            </div>
+            -----------------------------------------------*/
+
+            const commentBody = document.createElement("p");
+            commentBody.classList.add("comment-body");
+            commentBody.textContent = comment.contenido;
+            /*----------------------------------------------
+            <p class="comment-body">
+            ${comment.contenido}
+            </p>
+            -----------------------------------------------*/
+
+            const deleteButton = document.createElement("button");
+            deleteButton.classList.add("delete-btn");
+            deleteButton.textContent = "X";
+            deleteButton.addEventListener("click", function() {
+                deleteComment(comment.id);
+            });
+
+            /*----------------------------------------------
+            <button class="delete-btn">X</button>
+            -----------------------------------------------*/
+
+            
+            const commentDate = document.createElement("p");
+            commentDate.classList.add("comment-date");
+            commentDate.textContent = comment.fecha;
+            /*----------------------------------------------
+            <p class="comment-date">
+            ${comment-date}
+            </p>
+            -----------------------------------------------*/
+
+            commentDiv.appendChild(commentBody);        // Añade el contenido al contenedor del comentario
+            commentDiv.appendChild(deleteButton);       // Añade el botón de eliminar al contenedor del comentario
+
+            commentWrapper.appendChild(commentDiv);     // Añade el contenedor del comentario a su "envoltorio"
+            commentWrapper.appendChild(commentDate);    // Añade la fecha del comentario debajo del contenedor del comentario, dentro del "envoltorio"
+
+            commentContainer.prepend(commentWrapper);   // Añade la estructura del comentario a la caja de comentarios.
+        });
+    }
+
+    // Función para agregar nuevo comentario
+    function addComment(content) {
+        // Crear objeto con propiedades del nuevo comentario
+        const newComment = {
+            id: commentId,
+            contenido: content,
+            fecha: new Date().toLocaleString()  // Convierte la fecha y hora actuales en formato de texto
+        };
+
+        commentId++;
+        commentList.push(newComment);   // Inserta el nuevo comentario al arreglo
+
+        showComments(); // Cargar la lista de comentarios
+    }
+
+    // Función para eliminar un comentario en base a su id
+    function deleteComment(id) {
+        const opc = confirm('¿Estás seguro que deseas eliminar el comentario?');
+        if (!opc) return; // Si se da en 'Cancelar', no hace nada
+        for (let i = 0; i < commentList.length; i++) {
+            if (commentList[i].id === id) { // Si encuentra el comentario con el ID solicitado, lo eliminará de la lista
+                commentList.splice(i, 1);
+                break;  // Salir del ciclo for cuando ya se haya eliminado el elemento de la lista
+            }
+        }
+        showComments(); // Cargar la lista de comentarios
+    }
+
+    // Al enviar el formulario, se recuperará el contenido del input y se añadirá un nuevo comentario a la lista
+    commentForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita que se recargue la página al enviar el formulario
+
+        const commentText = commentInput.value.trim();
+        if (commentText === "") return; // Si el contenido del input está vacío, no hace nada
+
+        addComment(commentText);
+        commentInput.value = "";
+    });
+});
